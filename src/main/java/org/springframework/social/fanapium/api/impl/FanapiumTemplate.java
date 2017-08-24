2fdef510b622ea12dc53fb8e12bbea317fb1ae6a
Mohammad Hosseini <m.hosseini@rashasoft.ir>
2017-08-24 14:27:13 +0430

    fix fanapium grant type

--- src/main/java/org/springframework/social/fanapium/api/impl/FanapiumTemplate.java
+++ src/main/java/org/springframework/social/fanapium/api/impl/FanapiumTemplate.java
@@ -1,14 +1,23 @@
 package org.springframework.social.fanapium.api.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.social.fanapium.api.*;
 import org.springframework.social.fanapium.api.impl.json.FanapiumModule;
 import org.springframework.social.oauth2.AbstractOAuth2ApiBinding;
 import org.springframework.social.oauth2.OAuth2Version;
 import org.springframework.social.support.ClientHttpRequestFactorySelector;
+import org.springframework.social.support.HttpRequestDecorator;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
 
 public class FanapiumTemplate extends AbstractOAuth2ApiBinding implements Fanapium {
 
@@ -25,6 +34,7 @@ public class FanapiumTemplate extends AbstractOAuth2ApiBinding implements Fanapi
 
     public FanapiumTemplate(String clientId, String clientSecret, String accessToken) {
         super(accessToken);
+        registerOAuth2Interceptor(accessToken);
         this.clientId = clientId;
         this.clientSecret = clientSecret;
         this.accessToken = accessToken;
@@ -93,5 +103,36 @@ public class FanapiumTemplate extends AbstractOAuth2ApiBinding implements Fanapi
         chatOperations = new ChatTemplate(this, isAuthorized());
         userOperations = new UserTemplate(this, isAuthorized());
     }
+
+    private void registerOAuth2Interceptor(String accessToken) {
+        List<ClientHttpRequestInterceptor> interceptors = getRestTemplate().getInterceptors();
+        interceptors.add(new OAuth2TokenParameterRequestInterceptor(accessToken));
+        getRestTemplate().setInterceptors(interceptors);
+    }
+
+    private static final class OAuth2TokenParameterRequestInterceptor implements ClientHttpRequestInterceptor {
+        private final String accessToken;
+
+        public OAuth2TokenParameterRequestInterceptor(String accessToken) {
+            this.accessToken = accessToken;
+        }
+
+        public ClientHttpResponse intercept(final HttpRequest request, final byte[] body, ClientHttpRequestExecution execution) throws IOException {
+            HttpRequest protectedResourceRequest = new HttpRequestDecorator(request) {
+                @Override
+                public URI getURI() {
+                    return super.getURI();
+                }
+            };
+
+            // LinkedIn doesn't accept the OAuth2 Bearer token authorization header.
+            protectedResourceRequest.getHeaders().remove("Authorization");
+            protectedResourceRequest.getHeaders().add("_token_", accessToken);
+            protectedResourceRequest.getHeaders().add("_token_issuer_", "1");
+            return execution.execute(protectedResourceRequest, body);
+        }
+
+    }
+
 
 }
